4793bd95418caf53432eff170848e0a14d23b10f
Siddhesh-Mavrick <siddheshbhise04@gmail.com>
2024-02-20 11:07:15 +0530

    task 5 completed

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tic Tac Toe 1v1 game
-Tic tac toe in HTML, CSS, JS
--- game.js
+++ game.js
@@ -1,24 +1,14 @@
-// variable that switches players turn
 let switchPlayer = true;
-//check variable for a tie.
 let tieCheck = false;
-//reference to board element in the DOM
 const board = document.getElementsByClassName('board')[0];
-//reference to header that indicates who's the next player.
 const next = document.getElementById('player-msg');
-// 2d array that stores the current board's X and O values.
 let pos = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0]
 ];
-//store the winner's cells.
 let winnerPos = [];
 
-/*
-* Draw the cell with either an X or an O.
-* If the cell is filled already, then do nothing.
-*/
 function draw(e) {
   let cell = e.target;
   if (cell.textContent == "") {
@@ -40,17 +30,12 @@ function draw(e) {
     switchPlayer = !switchPlayer;
   }
 }
-/*
-* Executes a full check on the game board.
-* Returns true in case of a tie or a win, false otherwise.
-*/
+
 function checkGameState() {
-  //check if tie
   if (tieCheck >= 9) {
     next.textContent = 'Tie!!!';
     return true;
   }
-  //check vertical cells for a winner
   for (let col = 0; col < 3; col++) {
     let first = pos[0][col];
     let second = pos[1][col];
@@ -63,7 +48,6 @@ function checkGameState() {
     }
   }
 
-  //check horizontal cells for a winner
   for (let row = 0; row < 3; row++) {
     let first = pos[row][0];
     let second = pos[row][1];
@@ -76,20 +60,17 @@ function checkGameState() {
     }
   }
 
-  //check diagonal cells for a winner
   let topLeft = pos[0][0];
   let topRight = pos[0][2];
   let center = pos[1][1];
   let bottomLeft = pos[2][0];
   let bottomRight = pos[2][2];
-  // top left to bottom right
   if (center != 0 && center == topLeft && center == bottomRight) {
     if (center == 1) next.textContent = "Player 1 wins";
     else if (center == 2) next.textContent = "Player 2 wins";
     colorWinner(0, 0, 1, 1, 2, 2);
     return true;
   }
-  //bottom left to top right
   if (center != 0 && center == bottomLeft && center == topRight) {
     if (center == 1) next.textContent = "Player 1 wins";
     else if (center == 2) next.textContent = "Player 2 wins";
@@ -99,31 +80,26 @@ function checkGameState() {
   return false;
 }
 
-//Colors winner's cells(3).
 function colorWinner(row1, col1, row2, col2, row3, col3) {
   colorCell(row1, col1);
   colorCell(row2, col2);
   colorCell(row3, col3);
 }
 
-//Color a cell at pos[row,col] of the board (table).
 function colorCell(row, col) {
   let c = board.getElementsByTagName('tr')[row].children[col];
   c.style.backgroundColor = '#C5D86D';
 }
 
-//Function expression for when a cell is clicked.
 const cellClick = function(e) {
   draw(e);
 };
 
-//Stops the game rendering the table cells unclickable.
 function end() {
   next.style.color = '#D7263D';
   board.removeEventListener('click', cellClick, false);
 }
 
-//Resets the whole board and variables to their initial states.
 function clearBoard() {
   let cells = document.querySelectorAll('td');
   for (let i = 0; i < 9; i++) {
@@ -144,7 +120,6 @@ function clearBoard() {
 }
 
 document.getElementById('btn').addEventListener('click', function() {
-  //location.reload();
   clearBoard();
 }, false);
 
